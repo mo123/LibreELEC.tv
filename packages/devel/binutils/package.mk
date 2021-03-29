1bccb5a03e1359a63465a224e5a918af24ed535e
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-29 21:40:25 +0200

    binutils: build "ar"

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -69,6 +69,7 @@ make_target() {
   make -C bfd
   make -C opcodes
   make -C binutils strings
+  make -C binutils ar
 }
 
 makeinstall_target() {
@@ -79,4 +80,5 @@ makeinstall_target() {
 
   mkdir -p ${INSTALL}/usr/bin
     cp binutils/strings ${INSTALL}/usr/bin
+    cp binutils/ar      ${INSTALL}/usr/bin/ar
 }
