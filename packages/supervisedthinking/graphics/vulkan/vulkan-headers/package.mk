ef96e39e129d9e99cb337f01cd3e007fa94321f4
SupervisedThinking <supervisedthinking@gmail.com>
2021-03-29 22:22:52 +0200

    packages/supervisedthinking: added various "graphics" packages

---
+++ packages/supervisedthinking/graphics/vulkan/vulkan-headers/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-headers"
+PKG_VERSION="1.2.173"
+PKG_SHA256="97ccee5ad5250b12624a89fe7b234d90befdebf0ae88734a95654efa4af2675c"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host"
+PKG_LONGDESC="Vulkan Header files and API registry"
---
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="vulkan-loader"
+PKG_VERSION="1.2.173"
+PKG_SHA256="a64fdd5ba78ca6cd168bbcfed9b7fc24d1eac4146db4b795299d4ea92815e470"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers"
+PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libX11 libXrandr libxcb"
+  fi
+}
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
+                         -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+
+  # Mali Vulkan support
+  if [ ! "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_WSI_XCB_SUPPORT=Off \
+                             -DBUILD_WSI_XLIB_SUPPORT=Off"
+  fi
+}
