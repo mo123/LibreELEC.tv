5bd6261c3ed5d318ee7c5016da342fc508a17b0e
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-01 22:12:49 +0200

    mpg123-system: updated to 1.26.5

--- packages/supervisedthinking/multimedia/mpg123-system/package.mk
+++ packages/supervisedthinking/multimedia/mpg123-system/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="mpg123-system"
-PKG_VERSION="1.26.4"
-PKG_SHA256="081991540df7a666b29049ad870f293cfa28863b36488ab4d58ceaa7b5846454"
+PKG_VERSION="1.26.5"
+PKG_SHA256="502a97e0d935be7e37d987338021d8f301bae35c2884f2a83d59c4b52466ef06"
 PKG_LICENSE="LGPL-2.0-or-later"
 PKG_SITE="http://www.mpg123.org/"
 PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-${PKG_VERSION}.tar.bz2"
