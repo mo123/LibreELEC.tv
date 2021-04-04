9c239ca6a006d3a4558fef9a012d088d612170da
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-04 20:52:01 +0200

    duckstation: updated to git 5e2ecea

--- packages/supervisedthinking/emulation/libretro/duckstation/package.mk
+++ packages/supervisedthinking/emulation/libretro/duckstation/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="duckstation"
-PKG_VERSION="abb76310c4fcefb86fb72b08482c332492e70c27" #v0.1+
-PKG_SHA256="2b704e97226c7eb13d1f68445ac1adf2192c5b75ab3a3fe6314f5dc9ad799e40"
+PKG_VERSION="5e2ecea111e7c6a2249957a3bef6a0250fc89c8c" #v0.1+
+PKG_SHA256="faabb26e7c8e75d605a4d5ad741c609de227df58b507f92ee420e0d7ef5ecc9b"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/duckstation"
 PKG_URL="https://github.com/libretro/duckstation/archive/${PKG_VERSION}.tar.gz"
@@ -21,10 +21,11 @@ PKG_LIBNAME="duckstation_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
 
 pre_configure_target() {
-  PKG_CMAKE_OPTS_TARGET="-D BUILD_SDL_FRONTEND=OFF \
+  PKG_CMAKE_OPTS_TARGET="-D BUILD_NOGUI_FRONTEND=OFF \
                          -D BUILD_QT_FRONTEND=OFF \
                          -D BUILD_LIBRETRO_CORE=ON \
-                         -D ENABLE_DISCORD_PRESENCE=OFF"
+                         -D ENABLE_DISCORD_PRESENCE=OFF \
+                         -D USE_SDL2=OFF"
 }
 
 makeinstall_target() {
