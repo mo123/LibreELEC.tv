d30521df10fe6d154e20512f1416a5d32ace5e64
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-04 20:49:11 +0200

    vice-libretro: updated to git 69fa8c1

--- packages/supervisedthinking/emulation/libretro/vice-libretro/package.mk
+++ packages/supervisedthinking/emulation/libretro/vice-libretro/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vice-libretro"
-PKG_VERSION="8b419e55477e6fa9bcf647ba97964fc65ed20a19"
-PKG_SHA256="d64c8e330636aae4ccfc284254f89b78ecd67df6ceef7d18ab113d3072be0cfc"
+PKG_VERSION="69fa8c1c8a969efd0c31efd5b6205cde91751592"
+PKG_SHA256="50d49b8580fe6602024fa74eb31259e118da32a699e689e1416d3e24300ee841"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/vice-libretro"
 PKG_URL="https://github.com/libretro/vice-libretro/archive/${PKG_VERSION}.tar.gz"
