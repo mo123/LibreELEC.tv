3f3600f5d5a4a666394a0598dad5c6d72388c5be
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-05 18:41:26 +0200

    pcsx2: updated to git b60765e

--- packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
+++ packages/supervisedthinking/emulation/standalone/pcsx2/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="pcsx2"
-PKG_VERSION="048356a39334a5e62c72ed3a271b1a4dc259e202" #v1.7.0-dev
+PKG_VERSION="b60765e976f6b1c45117bbdd042f8d6698c63002" #v1.7.0-dev
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/PCSX2/pcsx2"
