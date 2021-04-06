262226c345fc44e1c57c52e068608df87cb3fbe4
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-06 14:10:36 +0200

    duckstation: updated to git dfed054

--- packages/supervisedthinking/emulation/libretro/duckstation/package.mk
+++ packages/supervisedthinking/emulation/libretro/duckstation/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="duckstation"
-PKG_VERSION="5e2ecea111e7c6a2249957a3bef6a0250fc89c8c" #v0.1+
-PKG_SHA256="faabb26e7c8e75d605a4d5ad741c609de227df58b507f92ee420e0d7ef5ecc9b"
+PKG_VERSION="dfed054455180c4f993a0114806d9c28913625b3" #v0.1+
+PKG_SHA256="bc22152e04868208e566d1021d9517373101813c9167ad86f64ec7590ca590bb"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/libretro/duckstation"
 PKG_URL="https://github.com/libretro/duckstation/archive/${PKG_VERSION}.tar.gz"
