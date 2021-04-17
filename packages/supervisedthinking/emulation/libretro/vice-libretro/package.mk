b4ebb0309f2c8d97399572458bfac253ffb29352
SupervisedThinking <supervisedthinking@gmail.com>
2021-04-17 21:44:12 +0200

    vice-libretro: updated to git 0b48284 / v3.5

--- packages/supervisedthinking/emulation/libretro/vice-libretro/package.mk
+++ packages/supervisedthinking/emulation/libretro/vice-libretro/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vice-libretro"
-PKG_VERSION="d9c6b7538fb0be29730b873d24318840c6e7ae99"
-PKG_SHA256="40e3940153a78adcead2589e799c008d07f2cc73b4b9098dd20b065d2c2ad202"
+PKG_VERSION="0b48284ec660fa4c808d58489c7bd1d65378aa12" # v3.5
+PKG_SHA256="6781666dad9f7cf43fa05292dc02665974d9e5d512fedd39fc43b4e72404865a"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://github.com/libretro/vice-libretro"
 PKG_URL="https://github.com/libretro/vice-libretro/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc"
-PKG_LONGDESC="Versatile Commodore 8-bit Emulator version 3.3 LIBRETRO WIP"
+PKG_LONGDESC="Port of VICE, the Versatile Commodore Emulator 3.5"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-sysroot"
 
