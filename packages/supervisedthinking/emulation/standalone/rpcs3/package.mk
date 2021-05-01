b914411a81f5b65ca6ed9611f4bd112930b3324d
SupervisedThinking <supervisedthinking@gmail.com>
2021-05-01 12:11:41 +0200

    rpcs3: updated to v0.0.16

--- packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
+++ packages/supervisedthinking/emulation/standalone/rpcs3/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="rpcs3"
-PKG_VERSION="dece8a69f66bd0fba7aa1a6e0aaa070577321935" # v0.0.15+
+PKG_VERSION="b8477a470f151ea46ec033b3c49987f5e8bec463" # v0.0.16
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://rpcs3.net"
