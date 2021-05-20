e46c40532ac9e19203f0d5a768a3ab51f42c3734
SupervisedThinking <supervisedthinking@gmail.com>
2021-05-21 00:11:11 +0200

    vulkan: updated to 1.2.178

--- packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.2.177"
-PKG_SHA256="3f02056f261efaf3d0d96dc1ca22b272baea2263c138c72a77c70515ba155857"
+PKG_VERSION="1.2.178"
+PKG_SHA256="cb519e7837ed2cd980a6e34fd8937c09556da99a6dbaaa5ed83d53843876b83c"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
--- packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
+++ packages/supervisedthinking/graphics/vulkan/vulkan-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="1.2.177"
-PKG_SHA256="1a37e351af97e0f9885457096523c7edb7bea029f031166de5cacfac72732761"
+PKG_VERSION="1.2.178"
+PKG_SHA256="70e79d64ef5c5c77b2c85adbb60e1850044f4f56a0549fcc4837404c8e2a7066"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
