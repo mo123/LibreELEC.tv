ee2e8ede465f0acece3722566c73aa912444de34
mo123 <4404677+mo123@users.noreply.github.com>
2021-09-13 19:17:11 +0200

    Add RTL8723DU Wifi Support
    Add RTL8723DU Wifi Support

---
+++ packages/linux-drivers/RTL8723DU/package.mk
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="RTL8723DU"
+PKG_VERSION="v5.13.4"
+PKG_SHA256=""
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/lwfinger/rtl8723du"
+PKG_URL="https://github.com/lwfinger/rtl8723du/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Realtek RTL8723DU Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+#PKG_TOOLCHAIN="make"
+
+pre_make_target() {
+  unset LDFLAGS
+#  pwd
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+}
